aca9a4545ede43ad35c403643ed327765279747b
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-08-29 17:29:34 +0100

    resource creation Endpoints: part 2

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,15 +7,3 @@ mongoose.connect(connectionURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-});
