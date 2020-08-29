c1a163d5fe92367db734f4f02daeafc48d4a5a45
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-08-29 16:54:03 +0100

    resource creation Endpoints: part 1

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 
 const connectionURL =
   "mongodb+srv://michal123:michal123@devconnector.6tf9d.mongodb.net/TaskManager?retryWrites=true&w=majority";
@@ -8,61 +7,6 @@ mongoose.connect(connectionURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-const User = mongoose.model("User", {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid");
-      }
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    validate(value) {
-      if (value.length <= 6) {
-        throw new Error("Password needs to be longer than 6 characters.");
-      }
-      if (value.toLowerCase().includes("password")) {
-        throw new Error("'password' cannot be used as a password.");
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be a positive number");
-      }
-    },
-  },
-});
-
-// const me = new User({
-//   name: "Bogdan",
-//   email: "bogdan@test.com",
-//   password: "qwertyu",
-// });
-
-// me.save()
-//   .then((response) => {
-//     console.log(response);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
 
 const Task = mongoose.model("Task", {
   description: {
@@ -75,17 +19,3 @@ const Task = mongoose.model("Task", {
     default: false,
   },
 });
-
-const task = new Task({
-  description: "Finish the course",
-  //   completed: false,
-});
-
-task
-  .save()
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
---
+++ src/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (value.length <= 6) {
+        throw new Error("Password needs to be longer than 6 characters.");
+      }
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("'password' cannot be used as a password.");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
+  },
+});
+
+module.exports = User;
