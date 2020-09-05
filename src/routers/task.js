e046b65f652a318b865d9c07bf8d2b78f8bdaab4
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-09-05 16:37:50 +0100

    the user/task relationship

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const Task = require("../models/task");
+const auth = require("../middleware/auth");
 
 const router = new express.Router();
 
-router.get("/tasks", async (req, res) => {
+router.get("/tasks", auth, async (req, res) => {
   try {
     const tasks = await Task.find({});
     res.status(200).send(tasks);
@@ -26,8 +27,12 @@ router.get("/tasks/:id", async (req, res) => {
   }
 });
 
-router.post("/tasks", async (req, res) => {
-  const task = new Task(req.body);
+router.post("/tasks", auth, async (req, res) => {
+  // const task = new Task(req.body);
+  const task = new Task({
+    ...req.body,
+    owner: req.user._id,
+  });
   try {
     await task.save();
     res.status(201).send(task);
