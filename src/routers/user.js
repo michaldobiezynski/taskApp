77072e8370ddf0e8334262bd294dacee6e503fe5
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-09-10 09:06:29 +0100

    auto-cropping and image formatting

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 var multer = require("multer");
+const sharp = require("sharp");
 
 const User = require("../models/user");
 const auth = require("../middleware/auth");
@@ -118,7 +119,12 @@ router.post(
   auth,
   upload.single("avatar"),
   async (req, res) => {
-    req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
+
     await req.user.save();
     res.send();
   },
@@ -144,7 +150,7 @@ router.get("/user/:id/avatar", async (req, res) => {
     if (!user || !user.avatar) {
       throw new Error();
     }
-    res.set("Content-Type", "image/jpg");
+    res.set("Content-Type", "image/png");
     res.send(user.avatar);
   } catch (error) {
     res.status(404).send(error);
