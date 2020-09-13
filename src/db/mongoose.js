7abc26fd77bd3fb4b958e4bec9b880dd890db72e
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-09-13 10:14:19 +0100

    enviroment variables

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 
-const connectionURL =
-  "mongodb+srv://michal123:michal123@devconnector.6tf9d.mongodb.net/TaskManager?retryWrites=true&w=majority";
-
-mongoose.connect(connectionURL, {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
--- src/emails/account.js
+++ src/emails/account.js
@@ -1,9 +1,6 @@
 const sgMail = require("@sendgrid/mail");
 
-const sendgridAPIKey =
-  "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";
-
-sgMail.setApiKey(sendgridAPIKey);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // sgMail.send({
 //   to: "dobiezynski@gmail.com",
