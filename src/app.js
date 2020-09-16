a730905d417e38e5d2aef3bbd0368c226b131d56
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-09-16 08:58:59 +0100

    testing an express application: part 2

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+require("./db/mongoose");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+
+const app = express();
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+module.exports = app;
