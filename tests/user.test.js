ef187c1574397b9e17e070ace6224ed8cf605767
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-09-16 09:23:18 +0100

    jest setup and teardown

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require("supertest");
 const app = require("../src/app");
+const User = require("../src/models/user");
+
+const userOne = {
+  name: "TestOne",
+  email: "testone@gmail.com",
+  password: "test111!",
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+});
 
 test("Should signup a new user", async () => {
   await request(app)
@@ -10,4 +22,24 @@ test("Should signup a new user", async () => {
       password: "MyPass777!",
     })
     .expect(201);
+});
+
+test("Should login existing user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: userOne.password,
+    })
+    .expect(200);
+});
+
+test("Should not login nonexisten user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: "badEmail@gmail.com",
+      password: "badpass123!",
+    })
+    .expect(400);
 });
