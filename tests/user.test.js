0fc5c148407bc72f0ad3c83fd5054a56cf79aa3a
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-09-17 20:35:47 +0100

    advanced assertions part 2

--- tests/user.test.js
+++ tests/user.test.js
@@ -47,13 +47,16 @@ test("Should signup a new user", async () => {
 });
 
 test("Should login existing user", async () => {
-  await request(app)
+  const response = await request(app)
     .post("/users/login")
     .send({
       email: userOne.email,
       password: userOne.password,
     })
     .expect(200);
+
+    const user = await User.findById(response.body.user._id)
+    expect(response.body.token).toBe(user.tokens[1].token)
 });
 
 test("Should not login nonexisten user", async () => {
@@ -76,11 +79,15 @@ test("Should not get profile for unauthenticated user", async () => {
   await request(app).get("/users/me").send().expect(401);
 });
 test("Should delete account for user", async () => {
-  await request(app)
+  const response = await request(app)
     .delete("/users/me")
     .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
     .send()
     .expect(200);
+    const user = await User.findById(userOneId)
+    expect(user).toBeNull()
+
+
 });
 
 test("Should not delete account  for unauthenticated user", async () => {
